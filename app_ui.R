cf3fe35e587ba047b2f14ef95861eac98f9fa985
mmk42 <mmk42@uw.edu>
2021-06-08 20:42:34 -0700

    added analysis

--- app_ui.R
+++ app_ui.R
@@ -67,7 +67,12 @@ page_1 <- tabPanel(
     
     mainPanel(    
       plotlyOutput(outputId = "cost_yearly"),
-      plotOutput(outputId = "cost_dist")
+      plotOutput(outputId = "cost_dist"),
+      p("For all cost types se can see that the change in cost is very linear. We can also see that the cost
+        has been consistently increasing from 2010 to 2020 for all cost types. In the future it may be worth 
+        eploring why the increase in cost is so linear. We can also see from the box 
+        plot that teh average total cost is around $12750 and that althoiugh there are a few very high outliers,
+        the distributions is skewed left.")
     )))
 
 cancerRate_page <- tabPanel(
